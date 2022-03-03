e399454bb373272ed947c7d00112a54a89882aef
Kartik <9415kks@gmail.com>
2022-03-03 21:46:15 +0530

    Adding the styles to the client

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,12 +1,14 @@
 import react from "react";
 import Post from "./Post/Post.js";
+import useStyles from "./styles";
 
 const Posts = () => {
+  const classes = useStyles();
   return (
     <>
       <h1>POSTS</h1>
-      <Post/>
-      <Post/>
+      <Post />
+      <Post />
     </>
   );
 };
