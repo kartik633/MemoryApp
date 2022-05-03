7d4d1006d72ef98807f493195e8f40eb2f771142
Kartik <9415kks@gmail.com>
2022-05-03 15:15:55 +0530

    Adding the backgroud to the body of index file

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import react from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import Post from "./Post/Post.js";
 import useStyles from "./styles";
@@ -7,14 +8,23 @@ const Posts = () => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
-  console.log(posts); 
+  console.log(posts);
 
-  return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post.id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
